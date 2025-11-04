a4dad4a6a5f1ebae85d5b0342116fd3654ba7b85
Noirrr <muhamadraflirabani@gmail.com>
2025-11-04 08:07:23 +0700

    fix redirect dashboard

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -19,14 +19,18 @@ const DashboardHeader = () => {
   const { push } = useRouter();
   const ListPage = pathname.split("/");
   const currentPage = pathname.split("/").pop();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  console.log("🚀 ~ DashboardHeader ~ session:", session);
 
   useEffect(() => {
-    if (!session?.user) {
+    if (status === "unauthenticated") {
       push("/");
     }
-  }, [session?.user, push]);
+  }, [status, push]);
 
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
